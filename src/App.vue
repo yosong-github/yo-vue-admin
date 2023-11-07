afd79ce66c5d82556f6e842c1ce0d1a9c5f853f5
yosong <2404559603@qq.com>
2023-11-07 15:29:05 +0800

    build: :package: 项目初始化

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
 -->
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
@@ -15,7 +16,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="You   did it!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
