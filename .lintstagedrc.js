78fe04e70d285e8b7825b31a79bc01ca2bedfcac
yosong <2404559603@qq.com>
2023-11-07 15:03:05 +0800

    chore:项目初始化

---
+++ .lintstagedrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+    '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
+    '*.vue': ['eslint --fix', 'prettier --write'],
+    '*.{scss,less,styl,html}': ['prettier --write']
+}
